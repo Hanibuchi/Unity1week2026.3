3956ccc9059137d585684bb466b44d594c65908b
Hanibuchi <hanibuchikouto003@gmail.com>
2026-03-20 22:22:01 +0900

    DialogueTriggerでプレイヤーの立つ位置の調整をしないオプションを追加

--- Assets/_Project/Scripts/DialogueTrigger.cs
+++ Assets/_Project/Scripts/DialogueTrigger.cs
@@ -6,6 +6,8 @@ public class DialogueTrigger : MonoBehaviour, IInteractable
 {
     [Header("Dialogue Configuration")]
     [SerializeField] private List<DialogueNode> dialogueNodes;
+    [Tooltip("会話時にプレイヤーの位置を自動で調整するかどうか")]
+    [SerializeField] private bool adjustPlayerPosition = true;
     [Tooltip("会話時にプレイヤーが立つ位置の距離")]
     [SerializeField] private float playerStandOffset = 1.5f;
     [Tooltip("プレイヤーが範囲に入った時に自動で会話を開始するかどうか")]
@@ -45,17 +47,20 @@ public class DialogueTrigger : MonoBehaviour, IInteractable
         {
             if (PlayerController.Instance != null)
             {
-                // NPCの向き (scale.x が正なら右向き(1)、負なら左向き(-1))
-                float npcFacingDir = Mathf.Sign(transform.localScale.x);
+                if (adjustPlayerPosition)
+                {
+                    // NPCの向き (scale.x が正なら右向き(1)、負なら左向き(-1))
+                    float npcFacingDir = Mathf.Sign(transform.localScale.x);
 
-                // NPCの向いている方向に offsetting したX座標
-                float targetX = transform.position.x + (npcFacingDir * playerStandOffset);
-                Vector2 targetPos = new Vector2(targetX, PlayerController.Instance.transform.position.y);
+                    // NPCの向いている方向に offsetting したX座標
+                    float targetX = transform.position.x + (npcFacingDir * playerStandOffset);
+                    Vector2 targetPos = new Vector2(targetX, PlayerController.Instance.transform.position.y);
 
-                // NPCが右向きならプレイヤーは左を向き、左向きなら右を向くように設定する
-                bool playerShouldFaceRight = npcFacingDir < 0;
+                    // NPCが右向きならプレイヤーは左を向き、左向きなら右を向くように設定する
+                    bool playerShouldFaceRight = npcFacingDir < 0;
 
-                PlayerController.Instance.SetPositionAndFacing(targetPos, playerShouldFaceRight);
+                    PlayerController.Instance.SetPositionAndFacing(targetPos, playerShouldFaceRight);
+                }
                 
                 // カメラにNPCの親または自身と、プレイヤーを両方映す
                 if (CameraController.Instance != null)
